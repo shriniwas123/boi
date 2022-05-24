a66b97d28635533b1ce40a66241789394704e43d
shriniwas123 <106058854+shriniwas123@users.noreply.github.com>
2022-05-24 20:02:49 +0530

    Update Array.java

--- myProject/src/array/Array.java
+++ myProject/src/array/Array.java
@@ -4,7 +4,7 @@ public class Array {
 	public static void main (String [] args) {
 	String nam[]={"Shri", " kul"," n  Boi"};
 	for(int i =0; i<nam.length;i++)
-		System.out.print(nam[i]);;
+		System.out.print(nam[i]);
 	
 	}
 
